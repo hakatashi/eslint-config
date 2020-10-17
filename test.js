90c01d49fe8fb7dc4019d4a30fc4c8aff5d5e3b7
Koki Takahashi <hakatasiloving@gmail.com>
2020-10-17 11:58:05 +0900

    Improve test to include doc link in its output

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+const config = require('./');
 const eslint = require('eslint');
 
 const arrayPlural = require('eslint-plugin-array-plural');
@@ -7,32 +8,31 @@ const node = require('eslint-plugin-node');
 const privateProps = require('eslint-plugin-private-props');
 const react = require('eslint-plugin-react');
 const vue = require('eslint-plugin-vue');
-const config = require('./');
 
 const linter = new eslint.Linter();
 
 const rules = [
-	...linter.getRules().keys(),
-	...Object.keys(react.rules).map((rule) => `react/${rule}`),
-	...Object.keys(arrayPlural.rules).map((rule) => `array-plural/${rule}`),
-	...Object.keys(privateProps.rules).map((rule) => `private-props/${rule}`),
-	...Object.keys(mysticatea.rules).map((rule) => `mysticatea/${rule}`),
-	...Object.keys(node.rules).map((rule) => `node/${rule}`),
-	...Object.keys(vue.rules).map((rule) => `vue/${rule}`),
-	...Object.keys(importPlugin.rules).map((rule) => `import/${rule}`),
+	...linter.getRules().entries(),
+	...Object.entries(react.rules).map(([name, rule]) => [`react/${name}`, rule]),
+	...Object.entries(arrayPlural.rules).map(([name, rule]) => [`array-plural/${name}`, rule]),
+	...Object.entries(privateProps.rules).map(([name, rule]) => [`private-props/${name}`, rule]),
+	...Object.entries(mysticatea.rules).map(([name, rule]) => [`mysticatea/${name}`, rule]),
+	...Object.entries(node.rules).map(([name, rule]) => [`node/${name}`, rule]),
+	...Object.entries(vue.rules).map(([name, rule]) => [`vue/${name}`, rule]),
+	...Object.entries(importPlugin.rules).map(([name, rule]) => [`import/${name}`, rule]),
 ];
 
 let isError = false;
 
-for (const rule of rules) {
-	if (config.rules[rule] === undefined) {
-		process.stdout.write(`ERROR: Config for the rule ${rule} is missing.\n`);
+for (const [name, rule] of rules) {
+	if (config.rules[name] === undefined) {
+		process.stdout.write(`ERROR: Config for the rule ${name} is missing. Doc: ${rule.meta.docs.url}\n`);
 		isError = true;
 	}
 }
 
 for (const rule of Object.keys(config.rules)) {
-	if (!rules.includes(rule)) {
+	if (!rules.some(([name]) => name === rule)) {
 		process.stdout.write(`ERROR: Config for the rule ${rule} is extraneous.\n`);
 		isError = true;
 	}
